867584c706b38b9f9521c254794575bdf208ab2d
Darina <darina@Vadims-MacBook-Pro-2.local>
2022-07-14 16:11:00 +0300

    fixed_existing_tests_and_created_new_eatstreet_tests

--- src/test/java/TestFindElementsXpath.java
+++ src/test/java/TestFindElementsXpath.java
@@ -3,6 +3,7 @@ import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import static org.testng.Assert.assertTrue;
@@ -22,8 +23,9 @@ public class TestFindElementsXpath {
     public void setUpChromeDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        sleep(2);
+        sleep(3);
         driver.get("https://eatstreet.com/");
+        sleep(3);
     }
 
     @AfterMethod
@@ -49,10 +51,12 @@ public class TestFindElementsXpath {
 
     @Test
     public void checkDeliveryTakeoutToggle(){
-        driver.findElement(By.xpath("//label[contains(text(), 'Delivery')]")).click();
-        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago");
-        driver.findElement(By.xpath("//input[@id='filters-checkbox-delivery']")).click();
-        assertTrue(driver.findElement(By.xpath("//*[@class='filters_radio_container_item ng-valid ng-not-empty ng-touched ng-dirty ng-valid-parse']")).isDisplayed());
+        driver.findElement(By.xpath("//label[@for='home-checkbox-takeout']")).click();
+        sleep(4);
+        driver.findElement(By.xpath("//label[@for='home-checkbox-delivery']")).click();
+        sleep(4);
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@id='home-checkbox-delivery']"))
+                .getAttribute("class").contains("ng-valid-parse"));
     }
 
     @Test
@@ -67,26 +71,47 @@ public class TestFindElementsXpath {
     @Test
     public void checkAfricanFoodFilter(){
         driver.findElement(By.xpath("//label[contains(text(), 'Delivery')]")).click();
-        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago");
+        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago\n");
+        driver.findElement(By.xpath("//*[@id='enter-address-btn']")).click();
+        driver.findElement(By.xpath("//*[@id='find-restaurants']")).click();
+        sleep(7);
         driver.findElement(By.xpath("//label[contains(text(), 'African Food')]")).click();
-        assertTrue(driver.findElement(By.xpath("//div[@class='restaurants-list_active-filters']")).isDisplayed());
+        assertTrue(driver.findElement(By.xpath("//div[@class='restaurants-list_active-filters']/div")).isDisplayed());
+//        driver.findElement(By.xpath("//label[contains(text(), 'African Food')]/../input")).isSelected()
     }
 
     @Test
-    public void checkSortByDeliveryActiveCategory() {
+    public void checkSortByDeliveryFeeCategory() {
         driver.findElement(By.xpath("//label[contains(text(), 'Delivery')]")).click();
-        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago");
+        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago\n");
+        driver.findElement(By.xpath("//*[@id='enter-address-btn']")).click();
+        driver.findElement(By.xpath("//*[@id='find-restaurants']")).click();
+        sleep(7);
         driver.findElement(By.xpath("//img[@alt='arrow-icon']")).click();
-        assertTrue(driver.findElement(By.xpath("//button[@aria-label='Sort by delivery_feeis active']")).isDisplayed());
+        driver.findElement(By.xpath("//button[@aria-label='delivery_fee']")).click();
+        assertTrue(driver.findElement(By.xpath("//button[contains(text(), ' Delivery Fee ')]")).isDisplayed());
     }
 
     @Test
     public void checkSortByRatingCategory(){
         driver.findElement(By.xpath("//label[contains(text(), 'Delivery')]")).click();
-        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago");
+        driver.findElement(By.xpath("//input[@id='input-food-search']")).sendKeys("Chicago\n");
+        sleep(3);
+        driver.findElement(By.xpath("//*[@id='enter-address-btn']")).click();
+        driver.findElement(By.xpath("//*[@id='find-restaurants']")).click();
+        sleep(7);
         driver.findElement(By.xpath("//img[@alt='arrow-icon']")).click();
         driver.findElement(By.xpath("//button[@aria-label='rating']")).click();
         driver.findElement(By.xpath("//img[@alt='arrow-icon']")).click();
         assertTrue(driver.findElement(By.xpath("//button[@aria-label='Sort by ratingis active']")).isDisplayed());
+    }
+
+    @Test
+    public void checkOpeningToledoKitchenOrder(){
+        driver.findElement(By.xpath("//button[contains(text(), 'Got it')]")).click();
+        sleep(4);
+        driver.findElement(By.xpath("//a[contains(text(), 'Toledo, OH')]")).click();
+        sleep(4);
+        Assert.assertTrue(driver.findElement(By.xpath("//a[contains(text(), 'View All Toledo Restaurants')]")).isDisplayed());
     }
 }
